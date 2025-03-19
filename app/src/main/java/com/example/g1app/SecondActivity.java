b1fa3168b226e25ebe53f93398777429a4cfbff9
hakou_dz2 <hakou.silem@hotmail.com>
2025-03-19 12:05:36 +0100

    recycleView

--- app/src/main/java/com/example/g1app/SecondActivity.java
+++ app/src/main/java/com/example/g1app/SecondActivity.java
@@ -11,6 +11,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -20,23 +25,26 @@ public class SecondActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_second);
 
-        TextView welcome = findViewById(R.id.tv_welcome);
+        RecyclerView recyclerView = findViewById(R.id.list_course);
 
-        Intent intent = getIntent();
+        List<Course> courses = new ArrayList<>();
+        courses.add(new Course("Course 1", 15.0));
+        courses.add(new Course("Course 2", 16.0));
+        courses.add(new Course("Course 3", 17.0));
+        courses.add(new Course("Course 4", 18.0));
+        courses.add(new Course("Course 5", 19.0));
+        courses.add(new Course("Course 6", 10.0));
+        courses.add(new Course("Course 7", 11.0));
+        courses.add(new Course("Course 8", 12.0));
+        courses.add(new Course("Course 9", 13.0));
+        courses.add(new Course("Course 10", 14.0));
 
-        String username = intent.getStringExtra("username");
+        CourseAdapter adapter = new CourseAdapter(courses);
 
-        welcome.setText("Welcome " + username );
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        Button logout= findViewById(R.id.btn_logout);
 
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent1 = new Intent(SecondActivity.this,MainActivity.class);
-                startActivity(intent1);
-            }
-        });
 
     }
 }
